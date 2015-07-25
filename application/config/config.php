d693ef14d919213fd53f1bdd9c29a53554649744
Bruno Marques Nogueira <developer@amareong.org.br>
2015-07-25 11:14:39 -0300

    Linguagem PT-Br no CI

--- application/config/config.php
+++ application/config/config.php
@@ -29,7 +29,7 @@ $config['base_url'] = '';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
@@ -71,7 +71,7 @@ $config['url_suffix'] = '';
 | than english.
 |
 */
-$config['language']	= 'english';
+$config['language']	= 'portuguese-br';
 
 /*
 |--------------------------------------------------------------------------
@@ -305,7 +305,7 @@ $config['cache_query_string'] = FALSE;
 | http://codeigniter.com/user_guide/libraries/encryption.html
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '_amare*696_';
 
 /*
 |--------------------------------------------------------------------------
